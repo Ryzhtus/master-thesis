559a79d9b96fca7332177fe590ae3135e258b333
Ryzhtus <shkunkov.a@yandex.ru>
2021-04-14 15:05:13 +0300

    Add new model with additional context that works as well as basic BERT without the modification of last hidden state

--- named_entity_recognition/model.py
+++ named_entity_recognition/model.py
@@ -80,3 +80,47 @@ class BertNERWithPreviousContextMean(nn.Module):
         predictions = self.linear(last_hidden_state)
 
         return predictions
+
+
+class MeanLastStateBertNERwithoutBERTtags(nn.Module):
+    def __init__(self, num_classes, device):
+        super(MeanLastStateBertNERwithoutBERTtags, self).__init__()
+        self.embedding_dim = 768
+        self.num_classes = num_classes
+        self.device = device
+
+        self.bert = BertModel.from_pretrained("bert-base-cased", output_hidden_states=True)
+        self.linear = nn.Linear(self.embedding_dim * 2, self.num_classes)
+
+    def forward(self, batch):
+        last_hidden_state = self.bert(batch)[0]
+
+        result_hidden_state = last_hidden_state.clone()
+        result_hidden_state = result_hidden_state.detach().cpu()
+
+        additional_context = torch.zeros_like(result_hidden_state, requires_grad=False)
+
+        token2embedding = {}
+
+        for batch_element_id, tokens in enumerate(batch):
+            for token_id, token in enumerate(tokens):
+                token = token.item()
+                if token in token2embedding.keys():
+                    token2embedding[token].append(result_hidden_state[batch_element_id][token_id])
+                else:
+                    token2embedding[token] = [result_hidden_state[batch_element_id][token_id]]
+
+        for token in token2embedding.keys():
+            token2embedding[token] = torch.mean(torch.stack(token2embedding[token]))
+
+        for batch_element_id, tokens in enumerate(batch):
+            for token_id, token in enumerate(tokens):
+                token = token.item()
+                additional_context[batch_element_id][token_id] = token2embedding[token]
+
+        additional_context = additional_context.to(self.device)
+        hidden_state_with_context = torch.cat((last_hidden_state, additional_context), 2)
+
+        predictions = self.linear(hidden_state_with_context)
+
+        return predictions
