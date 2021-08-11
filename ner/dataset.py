22f22ebf1577279f8851b3160e12024550335b4e
Ryzhtus <shkunkov.a@yandex.ru>
2021-08-11 16:50:35 +0300

    Add pytorch_lightning variant for training

--- ner/dataset.py
+++ ner/dataset.py
@@ -7,67 +7,57 @@ from torch.nn.utils.rnn import pad_sequence
 from transformers import PreTrainedTokenizer
 
 class CoNLLDatasetBERT(Dataset):
-    def __init__(self, sentences, tags, repeated_entities_masks, tokenizer):
+    def __init__(self, sentences, labels, repeated_entities_masks, tokenizer):
         self.sentences = sentences
-        self.sentences_tags = tags
+        self.labels = labels
         self.repeated_entities_masks = repeated_entities_masks
+        self.max_seq_length = 128
 
         self.tokenizer = tokenizer
 
-        self.ner_tags = [self.tokenizer.pad_token] + list(
-            set(tag for tag_list in self.sentences_tags for tag in tag_list))
-        self.tag2idx = {tag: idx for idx, tag in enumerate(self.ner_tags)}
-        self.idx2tag = {idx: tag for idx, tag in enumerate(self.ner_tags)}
+        self.entity_tags = sorted(list(set(tag for tag_list in self.labels for tag in tag_list)))
+        self.tag2idx = {tag: idx for idx, tag in enumerate(self.entity_tags)}
+        self.idx2tag = {idx: tag for idx, tag in enumerate(self.entity_tags)}
 
     def __len__(self):
         return len(self.sentences)
 
     def __getitem__(self, item):
         words = self.sentences[item]
-        tags = self.sentences_tags[item]
-        mask = self.repeated_entities_masks[item]
+        labels = self.labels[item]
 
-        word2mask = dict(zip(words, mask))
-        word2tag = dict(zip(words, tags))
+        word2tag = dict(zip(words, labels))
 
         tokens = []
         words_ids = []
-        tokenized_tags = []
-        tokenized_mask = []
+        tokenized_labels = []
 
         for word_id, word in enumerate(words):
             subtokens = self.tokenizer.tokenize(word)
             for i in range(len(subtokens)):
-                if i == 0:
-                    tokenized_tags.append(word2tag[word])
-                    tokenized_mask.append(word2mask[word])
-                else:
-                    tokenized_tags.append(-100)
-                    tokenized_mask.append(word2mask[word])
+                tokenized_labels.append(word2tag[word])
 
-                words_ids.append(word_id)
+            words_ids.append(len(tokens))
             tokens.extend(subtokens)
 
         tokens = [self.tokenizer.cls_token] + tokens + [self.tokenizer.sep_token]
         tokens_ids = self.tokenizer.convert_tokens_to_ids(tokens)
 
-        tags_ids = [-100] + [self.tag2idx[tag] if tag != -100 else tag for tag in tokenized_tags] + [-100]
+        label_ids = [-100] + [self.tag2idx[label] for label in tokenized_labels] + [-100]
 
-        tokenized_mask = [-1] + tokenized_mask + [-1]
-        attention_mask = [1 for token in tokens_ids]
+        attention_mask = [1 for _ in tokens_ids]
 
-        return torch.LongTensor(tokens_ids), torch.LongTensor(tags_ids), torch.LongTensor(tokenized_mask), \
-               torch.LongTensor(attention_mask), words_ids
+        return torch.LongTensor(tokens_ids), torch.LongTensor(label_ids), torch.LongTensor(attention_mask), \
+               words_ids
 
     def paddings(self, batch):
-        tokens, tags, masks, attention_masks, words_ids= list(zip(*batch))
+        tokens, tags, attention_masks, words_ids= list(zip(*batch))
 
         tokens = pad_sequence(tokens, batch_first=True, padding_value=0)
         tags = pad_sequence(tags, batch_first=True, padding_value=-100)
-        masks = pad_sequence(masks, batch_first=True, padding_value=0)
         attention_masks = pad_sequence(attention_masks, batch_first=True, padding_value=0)
 
-        return tokens, tags, masks, attention_masks, words_ids
+        return tokens, tags, attention_masks, words_ids
 
 
 class CoNLLDatasetT5(Dataset):
