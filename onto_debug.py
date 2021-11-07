5fcce6c9e0da0cbd959d3d6d6603c5b8fb8d592a
Arseniy Shkunkov <shkunkov.a@yandex.ru>
2021-11-07 17:20:18 +0300

    Bug fix OntoNotes dataset

--- onto_debug.py
+++ onto_debug.py
@@ -1,9 +1,10 @@
 from ner.dataset import ChunksPlusDocumentsDataset
 from ner.reader import ReaderOntonotes
+from ner.model import LongAttentionBERT
 
 from torch.utils.data import DataLoader
 
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BigBirdConfig
 
 BATCH_SIZE = 4
 SHUFFLE = False
@@ -17,4 +18,18 @@ sentences, labels, documents_masks, document2sentences, sentence2position = read
 dataset = ChunksPlusDocumentsDataset(sentences, labels, SEQ_LENGTH, document2sentences, sentence2position, tokenizer, 'Bert')
 dataloader = DataLoader(dataset, BATCH_SIZE, shuffle=SHUFFLE, collate_fn=dataset.paddings)
 
-print(dataset[0])
+input_ids, label_ids, attention_mask, word_ids = next(iter(dataloader))
+
+# количество классов для NER
+classes = len(dataset.entity_tags)
+
+config = BigBirdConfig()
+config.pad_token_id = 0
+config.bos_token_id = 101
+config.sep_token_id = 102
+config.eos_token_id = -1
+config.attention_type='original_full'
+
+model = LongAttentionBERT(model_name='bert-base-cased', classes=classes, attention_config=config)
+
+print(model.forward(input_ids=input_ids, attention_mask=attention_mask))
